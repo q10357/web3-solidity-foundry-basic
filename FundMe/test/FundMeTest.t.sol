2452a8f90a28df879d7d9f82cff2ee006a35a840
q10357 <isabel@chaparian.no>
2024-10-24 17:18:58 +0200

    added test fd testfile for FundMe smartcontract

---
+++ FundMe/test/FundMeTest.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        // This is run before each test function
+        fundMe = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.i_owner(), address(this));
+    }
+}
+
+// Verbosity levels:
+// - 2: Print logs for all tests
+// - 3: Print execution traces for failing tests
+// - 4: Print execution traces for all tests, and setup traces for failing tests
+
+// - 5: Print execution and setup traces for all tests
