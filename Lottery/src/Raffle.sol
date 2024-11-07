261cd1aff9db921933c3b3f1ceeb716d50ec84bd
q10357 <isabel@chaparian.no>
2024-11-07 23:07:45 +0100

    implemented interface VRFCoordinatorV2Plus, calls chainlink for random number to be used in winner draw

--- Lottery/src/Raffle.sol
+++ Lottery/src/Raffle.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+
+pragma solidity ^0.8.0;
 
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+import "forge-std/console.sol";
 
 // Layout of the contract file:
 // version
@@ -23,42 +26,54 @@ import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFCo
 // external
 // public
 // internal
+
 // private
 
 // view & pure functions
 
 error Raffle_NotEnoughEthSent();
+error Raffle_TransferFailed();
 
 contract Raffle is VRFConsumerBaseV2Plus {
-    uint256 private immutable i_entranceFee;
-    // @dev Duration of the raffle in seconds
-    uint256 private immutable i_interval;
-    address payable[] private s_players;
-    uint256 private s_lastTimeStamp;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 2;
 
     // The gas lane to use, which specifies the maximum gas price to bump to.
     // For a list of available gas lanes on each network,
     // see https://docs.chain.link/vrf/v2-5/supported-networks#configurations
-    bytes32 public s_keyHash =
+    bytes32 private immutable i_keyhash =
         0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit = 40000;
+    uint256 private immutable i_entranceFee;
+    // @dev Duration of the raffle in seconds
+    uint256 private immutable i_interval;
 
-    uint256 public s_subscriptionId;
-    uint32 public callbackGasLimit = 40000;
-
-    uint16 public requestConfirmations = 3;
-    uint32 public numWords = 1;
+    address payable[] private s_players;
+    address payable private s_recentWinner;
+    uint256 private s_lastTimeStamp;
 
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
+        i_keyhash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     // for users to enter
     function enterRaffle() external payable {
-        // require(msg.value >= i_entranceFee, "Not enough ETH!");
+        // require(msg.value >= i_entranceFee;, "Not enough ETH!");
         if (msg.value < i_entranceFee) revert Raffle_NotEnoughEthSent();
         s_players.push(payable(msg.sender));
         emit EnteredRaffle(msg.sender);
@@ -67,22 +82,39 @@ contract Raffle is VRFConsumerBaseV2Plus {
     // for system to pick winner
     function pickWinner() external {
         // has enough time passed?
-        if (block.timestamp - s_lastTimeStamp < i_interval) revert();
+        if (block.timestamp - s_lastTimeStamp < i_interval) {
+            revert("Not enough time passed");
+        }
 
         // Will revert if subscription is not set and funded.
-        requestId = s_vrfCoordinator.requestRandomWords(
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(
             VRFV2PlusClient.RandomWordsRequest({
-                keyHash: s_keyHash,
-                subId: s_subscriptionId,
-                requestConfirmations: requestConfirmations,
-                callbackGasLimit: callbackGasLimit,
-                numWords: numWords,
+                keyHash: i_keyhash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
                 extraArgs: VRFV2PlusClient._argsToBytes(
                     // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
                     VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
                 )
             })
         );
+        console.log(requestId);
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal virtual override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable winner = s_players[indexOfWinner];
+
+        s_recentWinner = winner;
+        // send the balance to the winner,l entire balance of this contract
+        (bool success, ) = winner.call{value: address(this).balance}("");
+
+        if (!success) revert Raffle_TransferFailed();
     }
 
     /* Getters */
