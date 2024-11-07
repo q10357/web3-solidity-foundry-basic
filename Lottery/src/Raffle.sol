018843b034888eee92a281828eaf255db59c52fc
q10357 <isabel@chaparian.no>
2024-11-07 21:01:42 +0100

    iknow there are errors, have to move this dir to wsl aspa

--- Lottery/src/Raffle.sol
+++ Lottery/src/Raffle.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
+
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 
 // Layout of the contract file:
 // version
@@ -27,12 +29,24 @@ pragma solidity ^0.8.0;
 
 error Raffle_NotEnoughEthSent();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
     uint256 private immutable i_entranceFee;
     // @dev Duration of the raffle in seconds
     uint256 private immutable i_interval;
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
+
+    // The gas lane to use, which specifies the maximum gas price to bump to.
+    // For a list of available gas lanes on each network,
+    // see https://docs.chain.link/vrf/v2-5/supported-networks#configurations
+    bytes32 public s_keyHash =
+        0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae;
+
+    uint256 public s_subscriptionId;
+    uint32 public callbackGasLimit = 40000;
+
+    uint16 public requestConfirmations = 3;
+    uint32 public numWords = 1;
 
     event EnteredRaffle(address indexed player);
 
@@ -54,6 +68,21 @@ contract Raffle {
     function pickWinner() external {
         // has enough time passed?
         if (block.timestamp - s_lastTimeStamp < i_interval) revert();
+
+        // Will revert if subscription is not set and funded.
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: s_keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
     }
 
     /* Getters */
